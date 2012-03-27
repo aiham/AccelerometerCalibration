6d592b1764651c82ff8e5e7e2383febbef1d4668
Aiham Hammami <aiham@aiham.net>
2012-03-27 21:47:15 +1100

    Initial Commit

---
+++ AccelerometerCalibration/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  AccelerometerCalibration
+//
+//  Created by Mohamad Aiham Hammami on 27/03/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
